ea81b329567598876f376147520283e144f6a178
DimiDimit <dmtrdmtrov@gmail.com>
2019-05-03 00:17:26 +0300

    Update README and add Water Bottle extinguishing you

---
+++ README.md
@@ -0,0 +1,4 @@
+# DAMTest
+A Minecraft mod for testing.
+
+For a list of additions, look at the [wiki](wiki).
--- src/main/java/net/dimitrodam/forgetest/DAMTest.kt
+++ src/main/java/net/dimitrodam/forgetest/DAMTest.kt
@@ -3,12 +3,15 @@ package net.dimitrodam.forgetest
 import net.minecraft.block.material.MapColor
 import net.minecraft.block.material.Material
 import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.SidedProxy
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
 import org.apache.logging.log4j.Logger
@@ -16,6 +19,7 @@ import org.apache.logging.log4j.Logger
 
 @Suppress("unused")
 @Mod(modid = DAMTest.MODID, name = DAMTest.NAME, version = DAMTest.VERSION, acceptedMinecraftVersions = DAMTest.ACCEPTED_MINECRAFT_VERSIONS, modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+@Mod.EventBusSubscriber
 object DAMTest {
 	const val MODID = "damtest"
 	const val NAME = "DimitrodAM Test"
@@ -30,13 +34,18 @@ object DAMTest {
 	lateinit var instance: DAMTest
 	lateinit var logger: Logger
 
+	//region Creative Tabs
 	val TAB_DT = object : CreativeTabs("damtest") {
 		@SideOnly(Side.CLIENT)
 		override fun getTabIconItem(): ItemStack = ItemStack(DTBlocks.fabricator)
 	}
+	//endregion
 
+	//region Materials
 	val MATERIAL_ROCK_HAND = Material(MapColor.STONE)
+	//endregion
 
+	//region Initialization Events
 	@JvmStatic
 	@Mod.EventHandler
 	fun preInit(event: FMLPreInitializationEvent) {
@@ -53,4 +62,16 @@ object DAMTest {
 	fun postInit(event: FMLPostInitializationEvent) {
 		proxy.postInit(event)
 	}
+	//endregion
+
+	//region Events
+	@JvmStatic
+	@SubscribeEvent
+	fun finishUseItem(event: LivingEntityUseItemEvent.Finish) {
+		val item = event.item
+		val entity = event.entityLiving
+		if(item.item == Items.POTIONITEM && item.tagCompound?.getString("Potion") == "minecraft:water" && entity.isBurning)
+			entity.extinguish()
+	}
+	//endregion
 }
